0a275fb40725100a4334cbd4345af158848d3916
John Doe <johndoe@email.com>
2022-03-24 09:34:04 +0700

    praktek demo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="App">
-    <h1>{{ message }}</h1>
+    <input type="text" v-model="variabelSatu" />
+    <input type="text" v-model="variabelDua" />
+    <button @click="lakukanPenjumlahan">Lakukan Penambahan</button>
+    <!-- <h1>{{ message }}</h1>
     <ol>
         <li v-for="item in items" v-bind:key="item">
             {{ item }}
@@ -9,28 +12,36 @@
 
       <input id="input" type="text" v-model="list">
         <button @click="addItem">Tambahkan</button>
-        <div v-if="items.length >= 4">Hebat!</div>
+        <div v-if="items.length >= 4">Hebat!</div> -->
   </div>
 </template>
 
 <script>
+import { penambahan } from '@/utils'
 
 export default ({
   name: 'App',
   data() {
     return {
-      message: "Ini adalah To Do List Ilmy",
-          list: "",
-          items: []
+      // message: "Ini adalah To Do List Ilmy",
+      //     list: "",
+      //     items: []
+      variabelSatu: 0,
+      variabelDua: 0,
+      hasil: 0
         }
       },
       methods:{
         addItem() {
-          if(this.list !=''){this.items.push( this.list )}
-          this.list='';
-                  }
+          // if(this.list !=''){this.items.push( this.list )}
+          // this.list='';
+          //         }
+          lakukanPenjumlahan(); {
+            this.hasil = penambahan(this.variabelSatu, this.variabelDua);
+          }
               }
-    })
+    }
+})
     
 </script>
 
