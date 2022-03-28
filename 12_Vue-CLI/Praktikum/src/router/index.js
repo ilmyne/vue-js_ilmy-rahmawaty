96a795f0509dbf1331d02458523a69ebefd06901
John Doe <johndoe@email.com>
2022-03-28 21:06:17 +0700

    belajar praktek dari video demo

---
+++ 12_Vue-CLI/Praktikum/src/router/index.js
@@ -0,0 +1,36 @@
+// import Vue from "vue";
+// import VueRouter from "vue-router";
+// import Home from "../views/Home.vue";
+// import About from "../views/About.vue";
+// import Contact from "../views/Contact.vue";
+// import { component } from "vue/types/umd";
+
+// Vue.use(VueRouter);
+
+// const routes = [
+//     {
+//         path: "/",
+//         name: "Home",
+//         component: Home,
+//     },
+//     {
+//         path: "/about",
+//         name: "About",
+//         component: () =>
+//         import("../views/About.vue"),
+//     },
+//     {
+//         path: "/contact",
+//         name: "Contact",
+//         component: () =>
+//         import("../views/Contact.vue"),
+//     },
+// ];
+
+// const router = new VueRouter({
+//     mode: "history",
+//     base: process.env.BASE_URL,
+//     routes,
+// });
+
+// export default router;
