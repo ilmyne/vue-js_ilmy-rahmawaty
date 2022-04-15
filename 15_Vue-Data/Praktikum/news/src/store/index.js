566b8fec639d5c3eca5daab663deade9727444b0
John Doe <johndoe@email.com>
2022-04-15 10:26:46 +0700

    submit praktikum

--- 15_Vue-Data/Praktikum/news/src/store/index.js
+++ 15_Vue-Data/Praktikum/news/src/store/index.js
@@ -1,17 +1,20 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
+import listBerita from "./listBerita";
 
-Vue.use(Vuex)
+
+Vue.use(Vuex);
+const persistedstate = createPersistedState({
+    paths: ["listBerita"],
+})
 
 export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+    plugins: [persistedstate],
+    modules: {
+        listBerita :{
+           namespaced: true,
+           ...listBerita,
+       },
+    },
+});
