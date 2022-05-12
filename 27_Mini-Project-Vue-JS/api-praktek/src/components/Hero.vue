278bf036bed6790da31834598ae587c56fd1a55d
John Doe <johndoe@email.com>
2022-05-12 16:16:08 +0700

    edit hero.vue

--- 27_Mini-Project-Vue-JS/api-praktek/src/components/Hero.vue
+++ 27_Mini-Project-Vue-JS/api-praktek/src/components/Hero.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="row">
       <div class="col">
-          <h2><strong>DevGear</strong><br></h2>
-          <p>Get your daily drive with best deal</p>
+          <h2><strong>Welcome</strong><br></h2>
+          <p>Place for your daily drive with best deal</p>
       </div>
       <img src="../assets/img/model1.jpg" width="100%"/>
   </div>
