bb0b7692cc82be74627431a69717ae2478d3925c
John Doe <johndoe@email.com>
2022-05-12 16:16:21 +0700

    adding hero2.vue

---
+++ 27_Mini-Project-Vue-JS/api-praktek/src/components/Hero2.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="row">
+        <div class="col">
+            <h2><strong>DevGear Product</strong><br></h2>
+        </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Hero2'
+}
+</script>
+
+<style>
+
+</style>
