026544d77656f842f2b9c39cb7d546ba7b795e71
John Doe <johndoe@email.com>
2022-05-12 16:17:57 +0700

    edit product.vue

---
+++ 27_Mini-Project-Vue-JS/api-praktek/src/views/Product.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="product">
+    <Navbar />
+    <div class="container">
+      <Hero2 />
+    </div>
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue'
+import Hero2 from '@/components/Hero2.vue'
+
+export default {
+  name: 'Product',
+  components: {
+    Navbar,
+    Hero2
+  }
+}
+</script>
+
+<style>
+
+</style>
