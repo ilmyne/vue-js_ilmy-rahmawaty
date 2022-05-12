504eff78b65f131de7decb1152ddee8ec833f245
John Doe <johndoe@email.com>
2022-05-12 16:17:42 +0700

    edit aboutview

--- 27_Mini-Project-Vue-JS/api-praktek/src/views/AboutView.vue
+++ 27_Mini-Project-Vue-JS/api-praktek/src/views/AboutView.vue
@@ -1,40 +1,23 @@
 <template>
-  <div id="SecondNews" class="container" style="max-width: 1400px">
-    <div class="App" v-for="(page, index) in item" :key="index">
-      <div class="container">
-        <table style="margin-top: 20px; margin-left: 100px">
-          <div>
-            <img :src="page.urlToImage" target="_blank" style="width: 500px; height: 400px" />
-            </div>
-          <h6>{{ page. author }}</h6>
-          <h5>{{ page. title }}</h5>
-          <h6>{{ page. publishedAt }}</h6>
-          <p>Beritanya menarik? <a :href="page.url">Klik disini</a></p>
-          <button class="btn btn-primary" @click="newsDetail">
-            Kembali
-          </button>
-        </table>
-      </div>
+  <div class="home">
+    <Navbar />
+    <div class="container">
+    <Hero />
+
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: "SecondNews" ,
-  methods: {
-    newsDetail() {
-      this.$router.push({ path: "/"});
-    },
-  },
-  computed: {
-    item() {
-      return this.$store.state.listItem;
-    },
-  },
-  mounted() {
-    this.$store.dispatch("fetchItem");
-  },
-};
+// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue'
+import Hero from '@/components/Hero.vue'
 
+export default {
+  name: 'AboutView',
+  components: {
+    Navbar,
+    Hero
+  }
+}
 </script>
