a13131d660c46978ec728dd9c9aba26458526e7b
John Doe <johndoe@email.com>
2022-07-24 21:40:21 +0700

    adding weekly task 1

---
+++ 26_Postest-Vue-JS/Weekly Task 1/src/components/section2.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="S2">
+        <h1>Version Control and Branch Management (Git)</h1>
+        <br>
+        <p class="pt-1 pl-5 pr-5">
+            Version Control untuk memperbarui versi dari sebuah project 
+            <br>
+            Salah satu Version Control System yang populer digunakan oleh programmer di seluruh dunia.
+
+Dengan GIT dapat berkolaborasi secara langsung.
+
+Pemanfaatan GIT dalam github adalah terdistribusi
+
+Lokal harus mengsinkronisasi ke server agar jika nanti server mengalami down, komputer lokal dapat mengedit tanpa kendala
+        </p>
+
+        <p class="pt-1 pl-5 pr-5">Latihan-Version Control and Branch Management (Git) <a href="https://docs.google.com/document/d/1nz3T5hTSC-C1TjsPnCvTCrkfzzJUEan4zOXxthfotTY/edit" target="_blank">klik disini</a></p>
+
+        <div>
+        <img class="pt-1 pl-5 pr-5 pb-5" style="width:70%" src="../assets/praktikumGit.png" alt="Git">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'section2Component',
+    setup() {
+        
+    },
+}
+</script>
